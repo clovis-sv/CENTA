4119303b1d7f8366bf440ca8d334a0a90f734fc6
clovis-sv <30555168+clovis-sv@users.noreply.github.com>
2017-08-06 18:26:14 +0200

    Add files via upload

---
+++ Data/launcher.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# launcher.sh
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/pi/projects/Data
+sudo python FS.py
+cd /
+
+
+
